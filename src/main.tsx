96a83c7c989312a9f78b8bf9e1257e0065ff1d39
Thilan Irshad <thilan.irshad@gmail.com>
2026-09-17 15:28:42 +0530

    Add React entrypoint

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx'
+import { InvitationProvider } from './context/InvitationContext.tsx'
+import './index.css'
+
+const basename =
+  import.meta.env.BASE_URL === '/'
+    ? undefined
+    : import.meta.env.BASE_URL.replace(/\/$/, '')
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter basename={basename}>
+      <InvitationProvider>
+        <App />
+      </InvitationProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
