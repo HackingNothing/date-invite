60ea001a6f96cd7dc5a1fe3598f1956029d545ee
Thilan Irshad <thilan.irshad@gmail.com>
2026-09-17 15:32:11 +0530

    Use HashRouter in production for static hosts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { InvitationProvider } from './context/InvitationContext.tsx'
 import './index.css'
@@ -10,12 +10,14 @@ const basename =
     ? undefined
     : import.meta.env.BASE_URL.replace(/\/$/, '')
 
+const Router = import.meta.env.PROD ? HashRouter : BrowserRouter
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <Router basename={import.meta.env.PROD ? undefined : basename}>
       <InvitationProvider>
         <App />
       </InvitationProvider>
-    </BrowserRouter>
+    </Router>
   </StrictMode>,
 )
